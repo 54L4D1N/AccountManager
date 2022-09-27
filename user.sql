e3376505dab7631060a146f395b9dc0e83fba9c8
54L4D1N <oliver.saladin@bbzbl-it.ch>
2022-09-27 14:27:00 +0200

    Database User import

---
+++ user.sql
@@ -0,0 +1,5 @@
+# Rechte für `work`@`localhost`
+
+GRANT USAGE ON *.* TO `work`@`localhost` IDENTIFIED BY PASSWORD '*C440999A1AD9CFECB1A9B4EF34DBAF603C5C6955';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON `accountmanager`.* TO `work`@`localhost`;
