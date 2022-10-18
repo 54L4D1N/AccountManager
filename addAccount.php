9c599eb135a34974ddfd20e3b88939f9b6f6d002
ByLidl <valentin.anderegg@bbzbl-it.ch>
2022-10-18 15:32:11 +0200

    updated nav bar

--- addAccount.php
+++ addAccount.php
@@ -92,6 +92,27 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 <body>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="admin.php">Session Handling</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <?php
+            if (isset($_SESSION['loggedin']) and $_SESSION['loggedin']) {
+                echo '<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>';
+            } else {
+                echo '<li class="nav-item"><a class="nav-link" href="register.php">Registrierung</a></li>';
+                echo '<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>';
+            }
+            ?>
+        </ul>
+    </div>
+</nav>
+
     <div class="container">
         <h1>Account hinzufuegen</h1>
         <?php
@@ -154,6 +175,4 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
-</body>
-
 </html>
--- admin.php
+++ admin.php
@@ -36,7 +36,7 @@ if (!isset($_SESSION['loggedin']) or !$_SESSION['loggedin']) {
 
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="register.php">Session Handling</a>
+    <a class="navbar-brand" href="admin.php">Session Handling</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
