5470046967fa686c7f00c4076a8810afcddc2b0e
ByLidl <valentin.anderegg@bbzbl-it.ch>
2022-10-24 20:33:37 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -3,13 +3,12 @@ require "db_connector.inc.php";
 
 session_start();
 // variablen initialisieren
-$error = $message = '';
+$error = '';
 
 if (!isset($_SESSION['loggedin']) or !$_SESSION['loggedin']) {
     // Session nicht OK,  Weiterleitung auf Anmeldung
     $error .= "Sie sind nicht angemeldet, melden Sie sich bitte auf der  <a href='login.php'>Login-Seite</a> an.";
 }
-
 
 if (!isset($_GET['select']))
     $error .= "Es wurde kein Konto ausgewählt!";
@@ -121,60 +120,58 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         // Ausgabe der Fehlermeldungen
         if (!empty($error)) {
             echo "<div class=\"alert alert-danger\" role=\"alert\">" . $error . "</div>";
-        } else if (!empty($message)) {
-            echo "<div class=\"alert alert-success\" role=\"alert\">" . $message . "</div>";
-        }
-        $query = "SELECT * FROM account WHERE id = ? and userid = ?";
-        $stmt = $mysqli->prepare($query);
-        $stmt->bind_param("ii", $_GET['select'], $_SESSION['id']);
-        $stmt->execute();
-        if ($res = $stmt->get_result()) {
-            if ($res->num_rows && $row = $res->fetch_assoc()) {
-                echo "<form action='' method='post'>
-        <div class='form-group'>
-            <label for='name'>Name</label>
-            <input type='text' name='name' class='form-control' id='name' maxlength='30' value='" . htmlspecialchars($row['name']) . "' placeholder='Geben Sie den Applikationsnamen an.'>
-        </div>
-        <div class='form-group'>
-            <label for='firstname'>Vorname</label>
-            <input type='text' name='firstname' class='form-control' id='firstname' maxlength='30' value='" . htmlspecialchars($row['firstname']) . "' placeholder='Geben Sie Ihren Vornamen an.'>
-        </div>
-        <div class='form-group'>
-            <label for='lastname'>Nachname</label>
-            <input type='text' name='lastname' class='form-control' id='lastname' maxlength='30' value='" . htmlspecialchars($row['lastname']) . "' placeholder='Geben Sie Ihren Nachnamen an'>
-        </div>
-        <div class='form-group'>
-            <label for='username'>Benutzername</label>
-            <input type='text' name='username' class='form-control' id='username' maxlength='30' value='" . htmlspecialchars($row['username']) . "' placeholder='Gross- und Keinbuchstaben, min 6 Zeichen.'>
-        </div>
-        <div class='form-group'>
-            <label for='password'>Password</label>
-            <input type='text' name='password' class='form-control' id='password' maxlength='255' value='" . htmlspecialchars($row['password']) . "' placeholder='Gross- und Kleinbuchstaben, Zahlen, Sonderzeichen, min. 8 Zeichen, keine Umlaute'>
-        </div>
-        <div class='form-group'>
-            <label for='email'>Email</label>
-            <input type='email' name='email' class='form-control' id='email' maxlength='100' value='" . htmlspecialchars($row['email']) . "' placeholder='Geben Sie Ihre Email-Adresse an.'>
-        </div>
-        <div class='form-group'>
-            <label for='link'>Link</label>
-            <input type='text' name='link' class='form-control' id='link' maxlength='255' value='" . htmlspecialchars($row['link']) . "' placeholder='Link für Webapplikation.'>
-        </div>
-        <div class='form-group'>
-            <label for='description'>Beschreibung</label>
-            <textarea name='description' class='form-control' id='description' rows='4' >" . htmlspecialchars($row['description']) . "</textarea>
-        </div>
-        <div class='form-group'>
-            <label for='comment'>Kommentar</label>
-            <textarea name='comment' class='form-control' id='comment' rows='2' >" . htmlspecialchars($row['comment']) . "</textarea>
-        </div>
-        <button type='submit' name='button' value='submit' class='btn btn-info'>Senden</button>
-        <button type='reset' name='button' value='reset' class='btn btn-warning'>Löschen</button>
-    </form>";
+        } else {
+            $query = "SELECT * FROM account WHERE id = ? and userid = ?";
+            $stmt = $mysqli->prepare($query);
+            $stmt->bind_param("ii", $_GET['select'], $_SESSION['id']);
+            $stmt->execute();
+            if ($res = $stmt->get_result()) {
+                if ($res->num_rows && $row = $res->fetch_assoc()) {
+                    echo "<form action='' method='post'>
+                        <div class='form-group'>
+                            <label for='name'>Name</label>
+                            <input type='text' name='name' class='form-control' id='name' maxlength='30' value='" . htmlspecialchars($row['name']) . "' placeholder='Geben Sie den Applikationsnamen an.'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='firstname'>Vorname</label>
+                            <input type='text' name='firstname' class='form-control' id='firstname' maxlength='30' value='" . htmlspecialchars($row['firstname']) . "' placeholder='Geben Sie Ihren Vornamen an.'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='lastname'>Nachname</label>
+                            <input type='text' name='lastname' class='form-control' id='lastname' maxlength='30' value='" . htmlspecialchars($row['lastname']) . "' placeholder='Geben Sie Ihren Nachnamen an'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='username'>Benutzername</label>
+                            <input type='text' name='username' class='form-control' id='username' maxlength='30' value='" . htmlspecialchars($row['username']) . "' placeholder='Gross- und Keinbuchstaben, min 6 Zeichen.'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='password'>Password</label>
+                            <input type='text' name='password' class='form-control' id='password' maxlength='255' value='" . htmlspecialchars($row['password']) . "' placeholder='Gross- und Kleinbuchstaben, Zahlen, Sonderzeichen, min. 8 Zeichen, keine Umlaute'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='email'>Email</label>
+                            <input type='email' name='email' class='form-control' id='email' maxlength='100' value='" . htmlspecialchars($row['email']) . "' placeholder='Geben Sie Ihre Email-Adresse an.'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='link'>Link</label>
+                            <input type='text' name='link' class='form-control' id='link' maxlength='255' value='" . htmlspecialchars($row['link']) . "' placeholder='Link für Webapplikation.'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='description'>Beschreibung</label>
+                            <textarea name='description' class='form-control' id='description' rows='4' >" . htmlspecialchars($row['description']) . "</textarea>
+                        </div>
+                        <div class='form-group'>
+                            <label for='comment'>Kommentar</label>
+                            <textarea name='comment' class='form-control' id='comment' rows='2' >" . htmlspecialchars($row['comment']) . "</textarea>
+                        </div>
+                        <button type='submit' name='button' value='submit' class='btn btn-info'>Senden</button>
+                        <button type='reset' name='button' value='reset' class='btn btn-warning'>Löschen</button>
+                    </form>";
+                }
             }
+            $stmt->close();
+            $mysqli->close();
         }
-        $stmt->close();
-        $mysqli->close();
-
         ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
