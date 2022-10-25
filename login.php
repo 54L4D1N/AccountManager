faa14fea6b5b414818dea4911bffae84c1cc9dcc
ByLidl <valentin.anderegg@bbzbl-it.ch>
2022-10-25 13:42:17 +0200

    Update login.php

--- login.php
+++ login.php
@@ -8,9 +8,9 @@ $message = '';
 
 // Formular wurde gesendet und Besucher ist noch nicht angemeldet.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	echo "<pre>";
-	print_r($_POST);
-	echo "</pre>";
+	//echo "<pre>";
+	//print_r($_POST);
+	//echo "</pre>";
 
 	$username = "";
 	// username
